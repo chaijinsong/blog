8cea17f0d0525576f107fee26b57ddc815d9f85b
chaijinsong <chaijinsong@didiglobal.com>
2021-07-26 08:56:20 +0800

    修改sh文件,输出pwd

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,14 @@ set -e
 # 生成静态文件
 npm run docs:build
 
+Cur_Dir=$(pwd)
+echo $Cur_Dir
+
 # 进入生成的文件夹
-cd ../docs/.vuepress/dist
+cd docs/.vuepress/dist
+
+Cur_Dir=$(pwd)
+echo $Cur_Dir
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
