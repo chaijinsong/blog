8d6f7e98905824d0aa9b79b6856ac67fd3883ddc
chaijinsong <chaijinsong@didiglobal.com>
2021-07-27 08:54:36 +0800

    push

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ git commit -m 'deploy'
 git remote add origin https://github.com/chaijinsong/chaijinsong.github.io.git
 
 # 如果发布到 https://<USERNAME>.github.io
-git push
+git push --set-upstream origin master
 # git push -f https://github.com/chaijinsong/chaijinsong.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
